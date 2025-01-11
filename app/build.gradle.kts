a95061ab2205ba96b73e679c1dd8a1bf8a7eaf46
Lâm Nguyễn <moclam1905@gmail.com>
2025-01-10 23:09:47 -0800

    try implementation KBloom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(project(":kbloom"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.kbloom)
 }
